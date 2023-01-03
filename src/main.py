42778414a3a7a769f70ed4e6861fd24cca32a540
nathan <nathan@jkt1.ebdesk.com>
2023-01-03 15:43:15 +0700

    feat::chart: initialization class service and call function

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
-from googletrans import Translator
 from loguru import logger
+from service.translate_service import TranslateApp
 from os import listdir
 
 if __name__ == "__main__":
-    path = "C:/Users/eBdesk/Documents/_NotWork/translate-app-srt/test"
-    for file in listdir('C:/Users/eBdesk/Documents/_NotWork/translate-app-srt/test'):
-        with open(f"{path}/{file}", 'r', encoding="utf8") as file_name:
+    path = "C:/Users/eBdesk/Documents/_NotWork/translate-app-srt/test/test-txt"
+    for file in listdir(path):
+        with open(f"{path}/{file}", 'r', encoding="utf8", newline='') as file_name:
             read = file_name.readlines()
-            for reader in read: 
-                logger.info(read)
+            trans_text = TranslateApp(read_text=read)
 
-    translator = Translator()
-    result = translator.translate(text="How much your money?", dest='id')
-    logger.info(result.text)
+            result_list = trans_text.iteration_reader()
+            for result in result_list:
+                logger.info(result.text)
